ad2a0ccbdfc4767e89dde61fa3b5bec03ae06c70
ByoungJae <bottlebaek5231@gmail.com>
2024-11-08 22:14:25 +0900

    init Setting

---
+++ src/test/java/com/example/security_jwt_project/SecurityJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_jwt_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
